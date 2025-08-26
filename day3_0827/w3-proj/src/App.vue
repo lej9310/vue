56ce26c942f4e5d3d9c348feed310ba5f5bcc393
lej9310 <lej9310@gmail.com>
2025-08-27 08:55:10 +0900

    8/26

---
+++ day3_0827/w3-proj/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+import Ex5 from './components/D5_event.vue';
+
+const pages = { Ex6 }
+const activeComp = ref("Ex1")
+</script>
+
+<template>
+  <button @click="activeComp = 'Ex6'">Methods</button>
+
+  <div id="comp_view">
+    <KeepAlive>
+      <component :is="pages[activeComp]"></component>
+    </KeepAlive>
+  </div>
+</template>
+
+<style scoped>
+button {
+  margin: 5px;
+  filter: drop-shadow(0 0 1.5px grey);
+}
+
+#comp_view {
+  margin-top: 20px;
+  border: 1px solid black;
+  border-radius: 10px;
+}
+</style>
