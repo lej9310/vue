32686b86eec6878ac55873c3be1b24e93c2166c8
lej9310 <lej9310@gmail.com>
2025-08-27 17:34:33 +0900

    8/27

--- day4_0827/w3-proj/src/App.vue
+++ day4_0827/w3-proj/src/App.vue
@@ -4,9 +4,13 @@ import Ex4 from './components/D4_for.vue';
 import Ex5 from './components/D5_events.vue';
 import Ex6 from './components/D6_methods.vue';
 import Ex7 from './components/D7_model.vue';
+import Ex8 from './components/D8_computed.vue';
+import Ex9 from './components/D9_watchers.vue';
+import Ex10 from './components/D10_Props.vue';
+import Ex11 from './components/D11_Forms.vue';
 
-const pages = { Ex4, Ex5, Ex6, Ex7}
-const activeComp = ref("Ex7")
+const pages = { Ex4, Ex5, Ex6, Ex7, Ex8, Ex9, Ex10, Ex11}
+const activeComp = ref("Ex10")
 </script>
 
 <template>
@@ -14,6 +18,10 @@ const activeComp = ref("Ex7")
   <button @click="activeComp = 'Ex5'">Events</button>
   <button @click="activeComp = 'Ex6'">Methods</button>
   <button @click="activeComp = 'Ex7'">v-model</button>
+  <button @click="activeComp = 'Ex8'">Computed Properties</button>
+  <button @click="activeComp = 'Ex9'">Watchers</button>
+  <button @click="activeComp = 'Ex10'">Props</button>
+  <button @click="activeComp = 'Ex11'">폼 입력 바인딩</button>
 
   <div id="comp_view">
     <KeepAlive>
--- day4_0827/w3-proj/src/components/D7_model.vue
+++ day4_0827/w3-proj/src/components/D7_model.vue
@@ -19,7 +19,7 @@
     </div>
     <hr>
 
-    <h4>Shopping List With Important Items</h4>
+    <h4>Mark Found Items in The Shopping List</h4>
     <div class="bg">
         <form @submit.prevent="addItem">
             <p>구매 목록:
@@ -35,28 +35,98 @@
                 </label>
             </p>
         </form>
-        <button type="submit" @click="addItem">품목 추가</button>        
+        <button type="submit" @click="addItem">품목 추가</button>
         <hr>
 
         <p><strong>[ 쇼핑 목록 ]</strong></p>
-        <ul>
-            <li v-for="item in shoppingList" v-bind:class="{ impClass: item.important }">
+        <ul id="ulToFind">
+            <li v-for="item in shoppingList" v-bind:class="{ impClass: item.important }"
+                @click="item.found = !item.found" v-show="!item.found">
                 품목명:{{ item.name }},<br>
-                갯수: {{ item.number }},<br></br>
+                갯수: {{ item.number }},<br>
+                중요도: {{ item.important }}
+            </li>
+        </ul>
+        <ul id="ulFound">
+            <li v-for="item in shoppingList" v-bind:class="{ impClass: item.important }"
+                @click="item.found = !item.found" v-show="item.found">
+                품목명:{{ item.name }},<br>
+                갯수: {{ item.number }},<br>
                 중요도: {{ item.important }}
             </li>
         </ul>
     </div>
     <hr>
 
-    <h4>Mark Found Items in The Shopping List</h4>
+    <h4>Use v-model to make The Form Itself Dynamic</h4>
     <div class="bg">
+        <form v-on:submit.prevent="addMenu">
+            <p>
+            <h4>[ 주문표 ]</h4>
+            <label>
+                <input type="radio" required value="Dinner" v-model="menuType" name="rgbType">
+                Dinner
+            </label><br>
+            <label>
+                <input type="radio" required value="Drink" v-model="menuType" name="rgbType">
+                Drink
+            </label><br>
+            <label>
+                <input typeof="radio" required value="Dessert" v-model="menuType" name="rgbType">
+                Dessert
+            </label><br>
+            </p>
+            <p v-show="itemType">
+                <label>
+                    <select required v-model="menuName" @change="newUrl">
+                        <option value="" selected disabled>메뉴를 고르세요.</option>
+                        <option v-for="menu in MenuList" v-bind:value="menu.name" v-show="menu.type === menuType"
+                            v-bind:data-url="menu.imgUrl">
+                            {{ menu.name }}
+                        </option>
+                    </select>
+                </label>
+            </p>
+            <img v-bind:src="menuUrl">
+            <p v-show="menuName">
+                <input type="number" placeholder="수량을 입력하세요." v-model="menuNumber" required>
+            </p>
+            <button type="submit">주문하기</button>
+        </form>
+        <br>
+        <hr>
+
+        <div>
+            <h4>[ 주문 내역 ]</h4>
+            <ul id="ulToFind">
+                <li v-for="menu in order">
+                    메뉴명: {{ menu.name }},
+                    수량: {{ menu.number }}
+                    <img v-bind:src="menu.url" alt="menu">
+                </li>
+            </ul>
+        </div>
+
     </div>
-    <hr>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+// import img_burrito from '../assets/img_burrito.svg'
+// import img_pizza from '../assets/img_pizza.svg'
+// import img_soup from '../assets/img_soup.svg'
+// import img_spaghetti from '../assets/img_spaghetti.svg'
+// import img_fish from '../assets/img_fish.svg'
+// import img_cake from '../assets/img_cake.svg'
+// import img_rice from '../assets/img_rice.svg'
+// import img_salad from '../assets/img_salad.svg'
+// import img_soda from '../assets/img_soda.svg'
+// import img_greenSoda from '../assets/img_greenSoda.svg'
+// import img_doughnut from '../assets/img_doughnut.svg'
+// import img_iceCream from '../assets/img_iceCream.svg'
+// import img_lemonade from '../assets/img_lemonade.svg'
+// import img_pancakes from '../assets/img_pancakes.svg'
+// import img_water from '../assets/img_water.svg'
 
 const inpText = ref('Initial Text')
 const important = ref(false)
@@ -69,14 +139,48 @@ const shoppingList = ref([
         name: 'Tomatoes',
         number: 5,
         important: false
+    },
+    {
+        name: 'Bread',
+        number: 1,
+        important: false
+    },
+    {
+        name: 'Soap',
+        number: 1,
+        important: true,
+        found: true
     }
+])
+
+const menuType = ref('')
+const menuName = ref('')
+const menuUrl = ref('')
+const menuNumber = ref(null)
+const MenuList = ref([
+    { name: 'Burrito', type: 'Dinner', imgUrl: img_burrito },
+    { name: 'Pizza', type: 'Dinner', imgUrl: img_pizza },
+    { name: 'Pho Soup', type: 'Dinner', imgUrl: img_soup },
+    { name: 'Spaghetti', type: 'Dinner', imgUrl: img_spaghetti },
+    { name: 'Fish', type: 'Dinner', imgUrl: img_fish },
+    { name: 'Cake', type: 'Dessert', imgUrl: img_cake },
+    { name: 'Rice', type: 'Dinner', imgUrl: img_rice },
+    { name: 'Salad', type: 'Dinner', imgUrl: img_salad },
+    { name: 'Coke', type: 'Drink', imgUrl: img_soda },
+    { name: 'Green Soda', type: 'Drink', imgUrl: img_greenSoda },
+    { name: 'Doughnut', type: 'Dessert', imgUrl: img_doughnut },
+    { name: 'Ice Cream', type: 'Dessert', imgUrl: img_iceCream },
+    { name: 'Lemonade', type: 'Drink', imgUrl: img_lemonade },
+    { name: 'Pancakes', type: 'Dessert', imgUrl: img_pancakes },
+    { name: 'Water', type: 'Drink', imgUrl: img_water }
 ])
 
 function addItem() {
     let item = {
         name: itemName.value,
         number: itemNumber.value,
-        important: itemImportant.value
+        important: itemImportant.value,
+        found: false
     }
     shoppingList.value.push(item)
     itemName.value = null
@@ -86,19 +190,15 @@ function addItem() {
 </script>
 
 <style scoped>
-div {
-    border: dashed greenyellow 1px;
+.bg {
     width: 90%;
+    min-height: 50px;
     padding: 5px;
     margin: 10px auto;
-    align-items: center;
-}
-
-.bg {
-    min-height: 50px;
     display: inline-block;
-    border: 3px double darkgoldenrod;
+    border: 3px double purple;
     background-color: lightgoldenrodyellow;
+    line-height: 1.5;
     font-weight: bold;
 }
 
@@ -111,10 +211,10 @@ label {
     padding: 5px;
 }
 
-label:hover {
+label:hover,
+li:hover {
     cursor: pointer;
     background-color: lightgrey;
-    border-radius: 5px;
 }
 
 button {
@@ -142,5 +242,10 @@ li {
     border: 2px dotted darkred;
     background-color: rgba(253, 139, 139, 0.61);
     font-weight: bold;
+}
+
+#ulFound li {
+    text-decoration: line-through;
+    background-color: lightgoldenrodyellow;
 }
 </style>
